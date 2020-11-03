2285d04457c5a2475cb1bffe98e1fd658e6d648c
michal.bialas-drzewiecki <mdrzewiecki91@gmail.com>
2020-11-03 22:55:29 +0100

    chore: docker-compose variables in env

--- homie/settings.py
+++ homie/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = 'homie.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
     }
